c18c2df72125c9f94f2d80b72989d6e9b007e7b4
Luis Alonso <00081220@uca.edu.sv>
2024-06-29 15:21:05 -0600

    refactor: api methods for supermarket logic

--- ExpressMarket/src/main/java/com/rponce/Ticketify/repositories/CategoryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.rponce.Ticketify.repositories;
-
-import org.springframework.data.repository.ListCrudRepository;
-
-import com.rponce.Ticketify.models.entities.Category;
-
-public interface CategoryRepository extends ListCrudRepository<Category, String>{
-	Category findByCategory(String category);
-}
